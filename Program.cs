8721cdf5e6048c7e30f044fa4ffe4fd9c5809b61
Igor <jaknikig@mail.ru>
2023-02-25 00:53:41 +0600

    init

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int numberEven = 2;
+while (numberEven <= number1)
+{
+    Console.Write(numberEven + " ");
+    numberEven = numberEven + 2;
+}
